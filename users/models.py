25bd4ad7d13bb70bfe8bf7c6f01e150b0ce21f5c
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-01-25 00:58:02 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username   = models.CharField(max_length=255, unique=True)
+    email       = models.EmailField(max_length=255, unique=True)
+    firstname   = models.CharField(max_length=255, unique=True)
+    lastname   = models.CharField(max_length=255, unique=True)
+    is_admin   = models.BooleanField(default=False) 
+    
+    USERNAME_FIELD = 'username' 
+    REQUIRED_FIELDS = []
+    
+    
+    class Meta:
+        ordering = ["pk"]
+
+    
