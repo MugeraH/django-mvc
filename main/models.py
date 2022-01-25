f8d8da4f17b463e017487f0da7039ad0e5470120
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-01-25 08:00:46 +0300

    set up access contorl for users

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+import sys
 
-# Create your models here.
+sys.path.append("..")
+from django.db import models
+from users.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        ordering = ["pk"]
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def filter_profile_by_id(cls, id):
+        profile = Profile.objects.filter(user__id = id).first()
+        return profile
